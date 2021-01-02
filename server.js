9a46a3a82908a0af55e0b164972455572ebfa295
zhmvictoryh <kexsaxy6klue@sina.com>
2021-01-02 22:54:52 +0800

    [SET] env.

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cors = require('cors');
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('Hello, Express!');
+})
+
+const PORT = process.env.PORT || 443;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
