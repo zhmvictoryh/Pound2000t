6eea5f03ddb6caf544445937dbb244f0f64bec86
zhmvictoryh <kexsaxy6klue@sina.com>
2021-06-11 11:54:24 +0800

    [Edit] CRUD diary.js

--- helpers/diary.js
+++ helpers/diary.js
@@ -9,7 +9,7 @@ SELECT diary_id, diary_date, title, good, bad, wish, create_date, update_date,
 diary_pic, user_id, feel_id FROM  diary
 */
 
-let sql  =  "SELECT D.diary_id, D.title, D.good, D.bad, D.wish, D.create_date, D.update_date " 
+let sql  =  " SELECT D.diary_id, D.title, D.good, D.bad, D.wish, D.create_date " 
 sql += ", D.user_id, F.feel_name "
 sql += " FROM  diary D LEFT JOIN Feel F ON D.feel_id = F.feel_id" 
 sql += " LEFT JOIN users U ON D.user_id = U.user_id WHERE u.user_id = " +json.user_id;
@@ -106,11 +106,11 @@ diary.list_allgood = async(json)=>{
     SELECT d.good, d.update_date FROM diary d LEFT JOIN users u ON d.user_id = u.user_id where 1=1
     */
     
-    let sql   =  "select  d.update_date, d.good"
+    let sql   =  " select  to_char(d.create_date, 'DD-MM-YYYY') as date, d.good"
         sql  +=  " from diary d " 
         sql  +=  "left join users u on d.user_id = u.user_id where u.user_id =" +json.user_id; 
-        sql  +=  " group by d.good,d.update_date"
-        sql  +=  " order by d.update_date desc;" 
+        sql  +=  " group by d.good,d.create_date"
+        sql  +=  " order by d.create_date desc;" 
     console.log(sql)   
     await psql.manyOrNone(sql)
                     .then((data) => {
@@ -142,11 +142,11 @@ diary.list_allbad = async(json)=>{
     SELECT d.bad, d.update_date FROM diary d LEFT JOIN users u ON d.user_id = u.user_id where 1=1
     */
     // แก้ syntax ไม่เอา hard code
-    let sql   =  "select  d.update_date, d.bad"
+    let sql   =  " select  to_char(d.create_date, 'DD-MM-YYYY') as date, d.bad"
         sql  +=  " from diary d " 
         sql  +=  "left join users u on d.user_id = u.user_id where u.user_id =" +json.user_id; 
-        sql  +=  " group by d.bad,d.update_date"
-        sql  +=  " order by d.update_date desc;" 
+        sql  +=  " group by d.bad,d.create_date"
+        sql  +=  " order by d.create_date desc;" 
         
     await psql.manyOrNone(sql)
                     .then((data) => {
@@ -177,11 +177,11 @@ diary.list_allbad = async(json)=>{
         SELECT d.wish, d.update_date FROM diary d LEFT JOIN users u ON d.user_id = u.user_id where 1=1
         */
         
-        let sql   =  "select  d.update_date, d.wish"
+        let sql   =  " select  to_char(d.create_date, 'DD-MM-YYYY') as date, d.wish"
             sql  +=  " from diary d " 
             sql  +=  "left join users u on d.user_id = u.user_id where u.user_id =" +json.user_id; 
-            sql  +=  " group by d.wish,d.update_date"
-            sql  +=  " order by d.update_date desc;" 
+            sql  +=  " group by d.wish,d.create_date"
+            sql  +=  " order by d.create_date desc;" 
             
         await psql.manyOrNone(sql)
                         .then((data) => {
